89809197541f114c59b3958df69a6618081668d9
ptkina <rara.sholehah12@gmail.com>
2026-05-20 03:22:45 +0700

    update join.js

--- js/join.js
+++ js/join.js
@@ -1,12 +1,59 @@
+import { db }
+from "./firebase.js";
+
+import {
+  
+  doc,
+  setDoc
+  
+}
+from "https://www.gstatic.com/firebasejs/12.3.0/firebase-firestore.js";
+
 const joinBtn =
   document.getElementById(
     "joinBtn"
   );
 
-joinBtn.onclick = () => {
+joinBtn.onclick = async () => {
+  
+  const name =
+    document.getElementById(
+      "name"
+    ).value;
+  
+  const room =
+    document.getElementById(
+      "room"
+    ).value;
+  
+  if (name === "" || room === "") {
+    
+    alert(
+      "Isi semua data!"
+    );
+    
+    return;
+    
+  }
+  
+  await setDoc(
+    
+    doc(
+      db,
+      "rooms",
+      room,
+      "players",
+      name
+    ),
+    
+    {
+      score: 0
+    }
+    
+  );
   
   alert(
-    "Tombol berhasil dipencet 😭🔥"
+    "Berhasil masuk room 😭🔥"
   );
   
 };
